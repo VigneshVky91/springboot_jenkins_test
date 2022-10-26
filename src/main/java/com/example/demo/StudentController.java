55e8db1fec0fd018bb6e8bbf78f5cf8b2e512597
Vignesh.manoharan <vignesh.manoharan@stackroute.in>
2022-10-26 16:11:30 +0530

    26th Oct 2022

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 	@GetMapping("/")
 	public String sayHello() {
-		return "Hello world...";
+		return "Hello world... Welcome";
 	}
 }
